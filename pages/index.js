e1192309c3f6b9ee158ff37e27aa443b896801e2
Lodreath <108182853+Lodreath@users.noreply.github.com>
2025-10-04 02:10:40 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oh my Gosh! You are so pretty!<p>You are so rock!</p></h1>
+    return <h1>Oh my Gosh! You are so notorius handsome!<p>You are so rock!</p></h1>
 } 
 
 export default Home;
